6e7e208946aeb3b822687d53cc46338d46777286
Fadel442 <fadelmuhammad4402@gmail.com>
2024-10-12 21:15:46 +0700

    commit kedua

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Modal from './components/komponen/Modal.vue';
+
+</script>
+
+<template>
+  <Modal :header="header" :text="text" theme="astral"/>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {Modal},
+    data() {
+      return {
+        title: 'Hello world Vue, ^^',
+        header: 'Honkai Star Rail',
+        text: 'Ambil tiket mu dan pergi menuju ke dunia luar',
+      }
+    },
+  }
+</script>
+
+<style>
+  #app {
+    text-align: center;
+    color: #2c3e50;
+  }
+  .container h1 {
+    color: black;
+    font-weight: lighter;
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding: 10px;
+  }
+</style>
+
+
---
+++ src/components/komponen/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="modal">
+        <div class="backdrop" :class="{express: theme === 'astral'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['header', 'text', 'theme']
+    }
+</script>
+
+<style scoped>
+    .backdrop p {
+        font-style: normal;
+    }
+    .modal {
+        top: 0;
+        position: fixed;
+        background: rgb(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .backdrop {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        color: white;
+        background: black;
+        border-radius: 10px;
+    }
+    .backdrop h1 {
+        color: white;
+    }
+    .backdrop.express {
+        background-color: crimson;
+        color: white;
+        font-style: italic;
+    }
+    .backdrop.backdrop.express h1 {
+        border-bottom: 1px solid #ddd;
+    }
+</style>
